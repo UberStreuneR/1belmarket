ec9525a0c9a981d3716763a9448229c871fa48f0
The Best User <C:\Users\Best User\AppData\Roaming\The Bat!>
2022-01-28 16:53:18 +0300

    React works

--- belmarket/settings.py
+++ belmarket/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing.apps.LandingConfig',
-    'market.apps.MarketConfig'
+    'market.apps.MarketConfig',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
---
+++ frontend/src/index.js
@@ -0,0 +1,2 @@
+import App from "./components/App";
+import Test from "./components/Test";
