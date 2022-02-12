96626d66a47636d5e771becb042174d62837efb3
The Best User <C:\Users\Best User\AppData\Roaming\The Bat!>
2022-02-12 15:00:45 +0300

    Fix

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
     path('', index),
     path('market/', index),
+    path('api/', include('market.urls'))
 
 ]
