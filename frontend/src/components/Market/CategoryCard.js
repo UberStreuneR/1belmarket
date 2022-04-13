5887ea683b832c87d75e8f34e9a9c7e686c29d6d
The Best User <C:\Users\Best User\AppData\Roaming\The Bat!>
2022-04-13 15:44:33 +0300

    Market somewhat done without clickability

--- frontend/src/components/Market/CategoryCard.js
+++ frontend/src/components/Market/CategoryCard.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {makeStyles} from "@mui/styles";
+import {Card, CardMedia, Typography} from "@mui/material";
 
-function CategoryCard(props) {
+const image = "https://source.unsplash.com/random";
+
+const useStyles = makeStyles({
+  categoryCard: {
+    height: "250px",
+    width: "250px",
+    borderRadius: "10px !important",
+    position: "relative",
+  },
+});
+
+
+function CategoryCard({ category }) {
+    const classes = useStyles();
     return (
-        <div></div>
+        <Card className={classes.categoryCard}>
+            <Typography variant="h4" sx={{ color: 'white', position: 'absolute', mt: 2, ml: 2 }}>{category.title}</Typography>
+            <CardMedia component="img" height="250" src={image} alt="Category image" />
+        </Card>
     );
 }
 
--- frontend/src/pages/Market.js
+++ frontend/src/pages/Market.js
@@ -1,10 +1,6 @@
 import React from "react";
-import PricingContent from "../components/TestComponents/PricingContent";
 import Items from "../components/Market/Items";
-import OptionsList from "../components/Market/OptionsList";
-import SearchBar from "../components/Market/SearchBar";
-import ItemHeader from "../components/Market/ItemHeader";
-import Footer from "../components/Market/Footer";
+
 export default function Market() {
   return <Items />;
 }
