6585442482567a8db7989e8e7db69b17520156de
supermistral <mediamart37@gmail.com>
2022-05-08 01:07:38 +0300

    Improved webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,20 +1,28 @@
-const path              = require("path");
-const webpack           = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const dotenv            = require("dotenv");
+const path                    = require("path");
+const webpack                 = require("webpack");
+const HtmlWebpackPlugin       = require("html-webpack-plugin");
+const dotenv                  = require("dotenv");
+const { CleanWebpackPlugin }  = require("clean-webpack-plugin");
+const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
+const CssMinimizerPlugin      = require("css-minimizer-webpack-plugin");
 
-const envFile = `.${process.env.NODE_ENV === "development" ? "dev" : "prod"}.env`;
+
+const DEBUG = process.env.NODE_ENV !== "production";
+const envFile = `.${DEBUG ? "dev" : "prod"}.env`;
 
 dotenv.config({
   path: path.resolve(__dirname, "..", envFile)
 });
 
+
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "[name].js",
+    publicPath: DEBUG ? '/' : '',
+    filename: "[name].[contenthash].bundle.js",
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -25,11 +33,32 @@ module.exports = {
         },
       },
       {
-        test: /\.(s[ac]ss|css)/,
-        use: [
-          'style-loader',
-          'css-loader'
+        test: /\.css$/,
+        use: DEBUG ? [
+          "style-loader",
+          "css-loader",
+        ] : [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "autoprefixer",
+                ],
+              }
+            }
+          }
         ]
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
       }
     ],
   },
@@ -41,14 +70,34 @@ module.exports = {
     port: 3000,
   },
   optimization: {
-    minimize: false,
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /node_modules/,
+          name: "vendors",
+          chunks: "all",
+        }
+      }
+    },
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(process.env),
     }),
+    new CleanWebpackPlugin(),
   ],
 };
