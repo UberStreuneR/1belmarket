1124cf16836ccfda62b2baa3af91bb7252838816
The Best User <C:\Users\Best User\AppData\Roaming\The Bat!>
2022-05-08 16:23:23 +0300

    Fixed references to deleted tested components

--- frontend/src/pages/Market.js
+++ frontend/src/pages/Market.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Items from "../components/Market/Items";
-import TestDialog from "../components/TestComponents/TestDialog";
 import store from "../components/Market/features/store";
 import { Provider } from "react-redux";
 export default function Market() {
-  // return <TestDialog />;
   return (
     <Provider store={store}>
       <Items />
